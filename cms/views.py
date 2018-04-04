0c87b7f06cfa852946cf94e1ffc5ec15275a2c61
Jesus Miguel Del Alamo Albiol <zylan@alpha24.aulas.gsyc.urjc.es>
2018-04-04 17:27:07 +0200

    terminado

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+from .models import Pages
+
+def barra(request):
+	lista = Pages.objects.all()
+	salida = "<ul>"
+	for page in lista:
+		salida += '<li><a href="page/' + str(page.id) + '">' + page.name + '</a>'
+	salida += "</ul>"
+	#salida += formulario
+
+	return HttpResponse(salida)
+
+@csrf_exempt
+def pages(request, numero):
+	if request.method == "POST":
+		g = Pages(name = request.POST['nombre'], page = request.POST['pagina'])
+		g.save()
+	try:
+		page = Pages.objects.get(id=int(numero))
+	except Grupo.DoesNotExist:
+		return HttpResponse("no existe")
+	return HttpResponse(page.name + " " + str(page.page))
--- myproject/urls.py
+++ myproject/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'cms.views.barra'),
+    url(r'^page/(\d+)$', 'cms.views.pages'),
     url(r'^admin/', include(admin.site.urls)),
 )
